a43cbce7e8fe3d2975d47921ab65fee16823e89b
sufyanahma <63979270+sufyanahma@users.noreply.github.com>
2020-04-23 02:16:37 +0500

    Update crack.py

--- crack.py
+++ crack.py
@@ -12,7 +12,7 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 br = mechanize.Browser()
 br.set_handle_robots(False)
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcesusor(),max_time=1)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
 br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
 
 
